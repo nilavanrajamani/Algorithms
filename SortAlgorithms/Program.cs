6616a199e6ade753d663981c1107a4d8b922181d
Nilavan Rajamani <nilavanrajamani@gmail.com>
2019-08-18 11:39:58 +0530

    Selction Sort

---
+++ SortAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectionSort selectionSort = new SelectionSort();
+            selectionSort.PerformSort();
+        }
+    }
+}
---
+++ SortAlgorithms/SelectionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms
+{
+    public class SelectionSort
+    {
+        public void PerformSort()
+        {
+            int[] arr = {20, 80, 10, 30, 60, 50,
+                     110, 100, 130, 170};
+
+            ReturnSortedArray(arr);
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+
+        private void ReturnSortedArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                //swap minIndex and i
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
